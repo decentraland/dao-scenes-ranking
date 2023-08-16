6d71be77be126f92d55a34b135626cc99514cf6e
Male-DL <131903423+Male-DL@users.noreply.github.com>
2023-08-16 16:05:11 -0300

    Update top_scenes_monthly.sql
    Signed-off-by: Male-DL <131903423+Male-DL@users.noreply.github.com>

--- top_scenes_monthly.sql
+++ top_scenes_monthly.sql
@@ -12,7 +12,7 @@ SELECT
     s.avg_days_returned,
     s.avg_daily_visits,
     s.avg_daily_unique_visitors
-FROM {{ 'scene_visit_duration' }} AS s
+FROM {{ 'scene_visit_aggregated' }} AS s
 QUALIFY row_number() OVER(
     PARTITION BY s.start_considered_dt, s.end_considered_dt
     ORDER BY s.avg_daily_visits DESC) <= 100
